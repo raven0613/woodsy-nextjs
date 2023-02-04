ca89ecb07f87d38d5cd8048f030c4aac1cf1db45
raven0613 <reevan0613@gmail.com>
2023-02-04 17:32:16 +0800

    feat: add user get collections api

--- pages/api/user/[id]/collections.ts
+++ pages/api/user/[id]/collections.ts
@@ -1,20 +1,19 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { Sequelize } from 'sequelize';
-import { Ihollow, Icomment, Iuser, errorMessage } from '../../../../type-config'
+import { Ihollow, Icomment, Iuser, errorMessage, successMessage } from '../../../../type-config'
 import db from '../../../../models/index';
 const DB: any = db;
-const { Users, Articles, Comments, Hollows } = DB;
-import bcrypt from 'bcrypt';
-const saltRounds = 10;
+const { Users, Articles, Comments, Hollows, Collections } = DB;
 
-export default function handleUser(req: NextApiRequest, res: NextApiResponse<Iuser | errorMessage> ) {
+function getOffset (page: number, limit: number) {
+  return (page - 1) * limit
+}
+
+export default function handleUserCollections(req: NextApiRequest, res: NextApiResponse<Iuser | errorMessage | successMessage> ) {
   switch (req.method) {
       case 'GET':
-          getUser(req, res)
-          break
-      case 'PUT':
-          editUser(req, res)
+          getUserCollections(req, res)
           break
       default:
           res.status(405).end() //Method Not Allowed
@@ -22,46 +21,21 @@ export default function handleUser(req: NextApiRequest, res: NextApiResponse<Ius
   }
 }
 
-async function editUser (req: NextApiRequest, res: NextApiResponse<Iuser | errorMessage>) {
-    const { id } = req.query
-    const idNum = Number(id)
-    const { name, account, email, password } = req.body
 
-    const t = await new Sequelize('woodsy_nextjs', 'root', process.env.SEQUELIZE_PASSWORD, {
-        host: 'localhost',
-        dialect: 'mysql'
-    }).transaction();
-
+async function getUserCollections (req: NextApiRequest, res: NextApiResponse<Iuser | errorMessage | successMessage>) {
+    const { page: p, limit: l, id } = req.query;
+    const page = Number(p), limit = Number(l), idNum = Number(id)
+    if (!id || !p || !l) return res.status(500).json({ error: '請確認請求資料' })
     try {
-        const editedPassword = await bcrypt.hash(password, saltRounds)
-
-        const user = await Users.findByPk(idNum, { transaction: t })
-        if (!user) return res.status(500).json({ error: '找不到使用者' })
-        
-        user.set({ name, account, email, editedPassword }, { transaction: t })
-        await user.save({ transaction: t })
-        await t.commit();
-
-        
-        res.status(200).json(user)
-    } catch (err) {
-        await t.rollback();
-        return res.status(500).json({ error: '伺服器錯誤' })
-    }
-}
-
-async function getUser (req: NextApiRequest, res: NextApiResponse<Iuser | errorMessage>) {
-    const { id } = req.query
-    const idNum = Number(id)
-    try {
-        // TODO: 之後要加上 articleCounts
-        const user = await Users.findByPk(idNum, {
-            attributes: ['id', 'name', 'account', 'email', 'role', 'createdAt', 'updatedAt'],
-            raw: true,
+        const collections = await Collections.findAll({
+            where: { user_id: idNum },
+            include: [
+                { model: Articles }],
+            limit,
+            offset: getOffset(page, limit),
             nest: true
         })
-        if (!user) return res.status(500).json({ error: '找不到使用者' })
-        res.status(200).json(user)
+        return res.status(200).json({ success: '成功取得紀錄', payload: collections }) 
     } catch (err) {
         return res.status(500).json({ error: '伺服器錯誤' })
     }
